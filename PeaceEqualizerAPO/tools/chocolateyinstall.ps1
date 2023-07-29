1aec0e177eb0ed4cdd7abb7021bcdf6c4d1645f4
Richard M <richard@rmoser.ca>
2023-07-29 08:47:57 -0600

    I thought I changed this.. xD

--- PeaceEqualizerAPO/tools/chocolateyinstall.ps1
+++ PeaceEqualizerAPO/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $packageArgs = @{
   Url64bit = 'https://sourceforge.net/projects/peace-equalizer-apo-extension/files/PeaceSetup.exe/download'
   Checksum64 = '1e2c5efda2b56843cf1850540ea1512701a9e2f24b0c1db6071a525ad75fe225'
   ChecksumType64 = 'sha256'
-  fileType      = 'msi'
-  silentArgs    = "INSTALLLEVEL=3 /quiet"
+  fileType      = 'exe'
+  silentArgs    = "/S"
 }
 
 Install-ChocolateyPackage @packageArgs
