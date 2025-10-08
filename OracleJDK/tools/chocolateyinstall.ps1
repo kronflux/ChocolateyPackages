4d38932d57e74be8ff304446b2281986c8f279aa
kronflux <richard@rmoser.ca>
2025-10-08 00:42:54 -0600

    Switch to workflows, update current versions

--- OracleJDK/tools/chocolateyinstall.ps1
+++ OracleJDK/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $packageArgs = @{
-  PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://download.oracle.com/java/20/latest/jdk-20_windows-x64_bin.msi'
-  Checksum64 = 'd6b4db0821789bffe1b6c88937d061e878e9773e2e7d6469d99c970c97e0d3f5'
-  ChecksumType64 = 'sha256'
-  fileType      = 'msi'
-  silentArgs    = "INSTALLLEVEL=3 /quiet"
+    PackageName   = $env:ChocolateyPackageName
+    Url64bit      = 'https://download.oracle.com/java/25/latest/jdk-25_windows-x64_bin.msi'
+    Checksum64    = 'ce82b5a97d3db992821d4a924c96cdc41495d85cdac1e673b6f4797784f94f5d'
+    ChecksumType64= 'sha256'
+    FileType      = 'msi'
+    SilentArgs    = "INSTALLLEVEL=3 /quiet"
 }
 
 Install-ChocolateyPackage @packageArgs
